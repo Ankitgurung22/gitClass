a915b2bf67c77ad665e4f4a97e6138694194c150
Ankit Gurung <ankit.gurung.4s18@icp.edu.np>
2020-01-10 22:24:39 +0545

    CWSubmission

--- APPLICATION DEVELOPMENT/APPLICATION DEVELOPMENT/MainWindow.xaml.cs
+++ APPLICATION DEVELOPMENT/APPLICATION DEVELOPMENT/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
+using APPLICATION_DEVELOPMENT;
 
 namespace CourseWorkSample
 {
@@ -25,115 +26,178 @@ namespace CourseWorkSample
         public MainWindow()
         {
             InitializeComponent();
-           // Startup();
+            Startup();
+
+            txtRegisNo.Text = read_from_file();//Registered regisno. readedfrom the saved file.
+           // txt_Date.Text = DateTime.Now.ToString("dd/MM/yyyy");//Extractes the actual date.
+           
         }
 
-       // public void Startup()
-        //{
-            /*var handler = new Handler();
-            var dataSet = handler.CreateDataSet();
-            AddSampleData(dataSet);
-            dataSet.WriteXmlSchema(@"F:\StudentCWSchema.xml");
-            dataSet.WriteXml(@"F:\StudentCWData.xml");*/
-
-            //var dataSet = new DataSet();
-            //dataSet.ReadXmlSchema(@"F:\StudentCWSchema.xml");
-            //dataSet.ReadXml(@"D:\StudentCWData.xml");
-
-            //var i = 0;
-       // } 
+       public void Startup()
+        {
+            var i = 0;//Variables initialized to 0.
+        } 
      
 
-       /* private void AddSampleData(DataSet dataSet)
+        private void AddSampleData(DataSet dataSet)
         {
-            var dr = dataSet.Tables["Course"].NewRow();
+            var dr = dataSet.Tables["Course"].NewRow();//Course table created.
 
             // Data extrated from the text Field//
             dr["Name"] = " txtName.Text";
             dr["Address"] = "txtAddress.Text";
             dr["ContactNo"] = " txtContact.Text ";
             dr["CourseEnroll"] = " txtCourseEnroll.Text ";
-            dr["RegsitrationDate"] = " txtRegisDate.Text ";
-
-
-
-            dataSet.Tables["Course"].Rows.Add(dr);
-
-            dr = dataSet.Tables["Course"].NewRow();
-            dr["Name"] = "Network & Communication";
-            dr["DisplayText"] = "BCA Network";
-            dataSet.Tables["Course"].Rows.Add(dr);
-
-            dr = dataSet.Tables["Course"].NewRow();
-            dr["Name"] = "Programming & Application Development";
-            dr["DisplayText"] = "BSc CSIT Application Development";
-            dataSet.Tables["Course"].Rows.Add(dr);
-
-            dr = dataSet.Tables["Student"].NewRow();
-            dr["Name"] = "Ishwor Sapkota";
-            dr["Address"] = "Kathmandu";
-            dr["ContactNo"] = "9851220845";
-            dr["CourseEnroll"] = 1;
-            dr["RegistrationDate"] = DateTime.Today.AddDays(-2);
-            dataSet.Tables["Student"].Rows.Add(dr);
-
-            dr = dataSet.Tables["Student"].NewRow();
-            dr["Name"] = "Samyam Sapkota";
-            dr["Address"] = "Kathmandu";
-            dr["ContactNo"] = "9851220846";
-            dr["CourseEnroll"] = 2;
-            dr["RegistrationDate"] = DateTime.Today.AddDays(-1);
-            dataSet.Tables["Student"].Rows.Add(dr);
-
-            dr = dataSet.Tables["Student"].NewRow();
-            dr["Name"] = "Safal Sapkota";
-            dr["Address"] = "Kathmandu";
-            dr["ContactNo"] = "9851220847";
-            dr["CourseEnroll"] = 3;
-            dr["RegistrationDate"] = DateTime.Today.AddDays(-3);
-            dataSet.Tables["Student"].Rows.Add(dr);
-
-        } */
-        private void AddSampleDataforstd(DataSet dataSet)
+            dr["RegsitrationDate"] = txt_date.SelectedDate.Value.ToString("MM/dd/yyyy");
+        } 
+      
+        private void AddofStdReport(DataSet dataSet)
         {
-            var dr = dataSet.Tables["Student"].NewRow();
-            dr["Name"] = "BBA";
-            dr["DisplayText"] = "BBA Hons";
-            dataSet.Tables["Course"].Rows.Add(dr);
 
+            var handler = new Handler();//New handler created for student report.
+            dataSet.Tables["StudentReport"].ReadXml(@"F:\StudentReport.xml");//Data stored in student report is readed.
 
-            var dr1 = dataSet.Tables["Student"].NewRow();
-
-            // Data extrated from the text Field//
-            dr1["Name"] =  txtName.Text;
+            var dr1 = dataSet.Tables["StudentReport"].NewRow();//New data row on the smae table is created.
+            dr1["RegisNo"] = txtRegisNo.Text;
+            dr1["Name"] = txtName.Text;
             dr1["Address"] = txtAddress.Text;
-            dr1["ContactNo"] =  txtContact.Text ;
-            dr1["CourseEnroll"] =  txtCourseEnroll.Text ;
-            dr1["RegsitrationDate"] =  txtRegisDate.Text ;
+            dr1["ContactNo"] = txtContact.Text;
+            dr1["CourseEnroll"] = txt_CourseEnroll.Text;
+            dr1["RegistrationDate"] = txt_date.SelectedDate.Value.ToString("MM/dd/yyyy");
+            dataSet.Tables["StudentReport"].Rows.Add(dr1);
+
+            dataSet.Tables["StudentReport"].WriteXml(@"F:\StudentReport.xml");//All data added to the StudentReport through variable dr1.
 
 
-
-           
         }
+        private void AppendStdReport(DataSet dataSet)
+        {
+                var handler = new Handler();//New handler created for data to be appended on student report.
 
+                dataSet.Tables["StudentReport"].ReadXml(@"D:\StudentReport.xml");
+           
+                var dr2 = dataSet.Tables["StudentReport"].NewRow();
+                dr2["RegisNo"] = txtRegisNo.Text;
+                dr2["Name"] = txtName.Text;
+                dr2["Address"] = txtAddress.Text;
+                dr2["ContactNo"] = txtContact.Text;
+                dr2["CourseEnroll"] = txt_CourseEnroll.Text;
+                dr2["RegsitrationDate"] = txt_date.SelectedDate.Value.ToString("MM/dd/yyyy");
+
+            dataSet.Tables["StudentReport"].Rows.Add(dr2);
+            dataSet.Tables["StudentReport"].WriteXml(@"F:\StudentReport.xml");//All data added to the StudentReport through variable dr2.
+
+        }
 
         private void Btn1_Click(object sender, RoutedEventArgs e)
         {
-            /*txtName.Text = "Ram";
-            txtAddress.Text = "Birauta";
-            txtContact.Text = "9816144631";
-            txtCourseEnroll.Text = "Computing";
-            txtRegisDate.Text = "2076/1/8";*/
+            if (txtName.Text == "")
+            {
+                String check1 = string.Empty;
+                MessageBox.Show("ERROR!!! PLEASE, Fill UP REQUIRED DETAILS!!!");
+                return;
+            }
+            else if (txtAddress.Text == "")
+            {
+                String check2 = string.Empty;
+                MessageBox.Show("ERROR!!! PLEASE, Fill UP REQUIRED DETAILS!!!");
+                return;
+            }
+            else if (txtContact.Text == "")
+            {
+                String check2 = string.Empty;
+                MessageBox.Show("ERROR!!!PLEASE, Fill UP REQUIRED DETAILS!!!");
+                return;
+            }
+            else if (txt_CourseEnroll.Text == "")
+            {
+                String check2 = string.Empty;
+                MessageBox.Show("ERROR!!!PLEASE, Fill UP REQUIRED DETAILS!!!");
+                return;
+            }
+            
+
+
             var handler = new Handler();
             var dataSet = handler.CreateDataSet();
-            //AddSampleData(dataSet);
-            AddSampleDataforstd(dataSet);
-            dataSet.WriteXmlSchema(@"F:\StudentCWSchema.xml");
-            dataSet.WriteXml(@"F:\StudentCWData.xml");
+
+            AddofStdReport(dataSet);
+
+
+            var regno = txtRegisNo.Text;
+            var name = txtName.Text;
+           
+            dataSet.Tables["Student"].WriteXml(@"F:\files\"+name+""+regno+".xml");
+
+
+            write_to_file(txtRegisNo.Text);
+
+            txtRegisNo.Text = read_from_file();  
+
+
+            ClearControls();
+            MessageBox.Show("Student details is Stored!!");
 
         }
 
         private void txtName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        private void write_to_file(string text)
+        { 
+            System.IO.File.WriteAllText(@"F:\count.txt", text);
+        }
+        private string read_from_file()
+        {
+
+            string text = System.IO.File.ReadAllText(@"F:\count.txt");
+
+            int i;
+
+            i = int.Parse(text.ToString());
+            i = i + 1;
+
+            return i.ToString();
+
+        }
+
+        private void ClearControls()
+        {
+            txtName.Text = "";
+            txtAddress.Text = "";
+            txtContact.Text = "";
+            //txt_Date.Text = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+      
+        private void LoadDataforReport1()
+        {
+            var handler = new Handler();
+            var dataSet = new DataSet();
+
+            var dr2 = dataSet.Tables["WeeklyReport"].NewRow();
+            dr2["RegisNo"] = txtRegisNo.Text;
+            dr2["Name"] = txtName.Text;
+            dr2["Address"] = txtAddress.Text;
+            dr2["ContactNo"] = txtContact.Text;
+            dr2["CourseEnroll"] = txt_CourseEnroll.Text;
+            dr2["RegsitrationDate"] = txt_date.SelectedDate.Value.ToString("MM/dd/yyyy");
+            dataSet.Tables["StudentReport"].Rows.Add(dr2);//All data added to the student through variable dr2.
+        }
+
+        private void Retrieve_Button(object sender, RoutedEventArgs e)
+        {
+            RetriveWindow Ret = new RetriveWindow();
+            Ret.Show();
+            Close();
+            MessageBox.Show("Student details is retrived");
+
+        }
+
+        private void txt_CourseEnroll_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
--- APPLICATION DEVELOPMENT/Datahandler/Handler.cs
+++ APPLICATION DEVELOPMENT/Datahandler/Handler.cs
@@ -8,13 +8,15 @@ namespace DataHandler
         public DataSet CreateDataSet()
         {
             var ds = new DataSet();
+
             ds.Tables.Add(CreateCourseTable());            
             ds.Tables.Add(CreateStudentTable());
-            ForeignKeyConstraint courseWorkFK = new ForeignKeyConstraint("courseWorkFK",
+            ds.Tables.Add(CreateStudentReportTable());
+           /* ForeignKeyConstraint courseWorkFK = new ForeignKeyConstraint("courseWorkFK",
             ds.Tables["Course"].Columns["ID"],
             ds.Tables["Student"].Columns["CourseEnroll"]);
             courseWorkFK.DeleteRule = Rule.None;
-            ds.Tables["Student"].Constraints.Add(courseWorkFK);
+            ds.Tables["Student"].Constraints.Add(courseWorkFK);*/
             return ds;
         }
 
@@ -27,12 +29,12 @@ namespace DataHandler
             dataColumn.AutoIncrementStep = 1;
 
             dt.Columns.Add(dataColumn);
-
-            dt.Columns.Add("Name", typeof(string));
-            dt.Columns.Add("Address", typeof(string));
-            dt.Columns.Add("ContactNo", typeof(string));
+            dt.Columns.Add("RegisNo", typeof(String));
+            dt.Columns.Add("Name", typeof(String));
+            dt.Columns.Add("Address", typeof(String));
+            dt.Columns.Add("ContactNo", typeof(String));
             dt.Columns.Add("CourseEnroll", typeof(int));
-            dt.Columns.Add("RegistrationDate", typeof(DateTime));
+            dt.Columns.Add("RegistrationDate", typeof(String));
             dt.PrimaryKey = new DataColumn[] { dt.Columns["ID"] };
             return dt;
         }
@@ -46,10 +48,33 @@ namespace DataHandler
             dataColumn.AutoIncrementStep = 1;
             dt.Columns.Add(dataColumn);
 
-            dt.Columns.Add("Name", typeof(string));
-            dt.Columns.Add("DisplayText", typeof(string));
+            dt.Columns.Add("Name", typeof(String));
+            dt.Columns.Add("DisplayText", typeof(String));
             dt.PrimaryKey = new DataColumn[] { dt.Columns["ID"] };
             return dt;
         }
+        private DataTable CreateStudentReportTable()
+        {
+            var dt = new DataTable("StudentReport");
+            DataColumn dataColumn = new DataColumn("ID", typeof(int));
+            dataColumn.AutoIncrement = true;
+            dataColumn.AutoIncrementSeed = 1;
+            dataColumn.AutoIncrementStep = 1;
+
+            dt.Columns.Add(dataColumn);
+
+            dt.Columns.Add("RegisNo", typeof(String));
+            dt.Columns.Add("Name", typeof(String));
+            dt.Columns.Add("Address", typeof(String));
+            dt.Columns.Add("ContactNo", typeof(String));
+            dt.Columns.Add("CourseEnroll", typeof(String));
+            dt.Columns.Add("RegistrationDate", typeof(String));
+            
+
+
+            dt.PrimaryKey = new DataColumn[] { dt.Columns["ID"] };
+            return dt;
+        }
+
     }
 }
